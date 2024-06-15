de3468a728841ffd0a89ab2ce196e893993b5bac
SankalpBajpaiGit <154622097+SankalpBajpaiGit@users.noreply.github.com>
2024-06-15 14:56:48 +0530

    Update jsp.js

--- jsp.js
+++ jsp.js
@@ -1,1 +1,25 @@
-
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+console.log(buttons);
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click' , function(e){
+      console.log(e)
+      console.log(e.target)
+      const s = e.target.id;
+      switch (s){
+        case 'grey':
+          body.style.backgroundColor = s;
+          break;
+      case 'white':
+          body.style.backgroundColor = s;
+          break;
+      case 'blue':
+          body.style.backgroundColor = s;
+          break;
+      case 'yellow':
+          body.style.backgroundColor = s;
+          break;
+      }
+    })
+});
